edb4c4406b7967624f85f4933a256f5286f46920
Andrew Fillenwarth <andyf016@gmail.com>
2020-08-20 14:33:35 -0400

    added model, migrated, working on views

--- ghostapp/models.py
+++ ghostapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    BOOL_CHOICES = ((True, 'Boast'), (False, 'Roast'))
+    boast = models.BooleanField(choices=BOOL_CHOICES, default=False)
+    post_content = models.CharField(max_length=140)
+    up_votes = models.IntegerField()
+    down_votes = models.IntegerField()
+    time_stamp = models.DateTimeField(default=timezone.now)
+
--- ghostapp/views.py
+++ ghostapp/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from ghostapp.models import Post
 
-# Create your views here.
+def index(request):
+    all_posts = Post.objects.all()
+    return render(request, 'index.html', {"posts": all_posts})
